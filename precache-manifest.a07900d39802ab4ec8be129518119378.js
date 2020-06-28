2c2e4046aab6ea770ba34f7cd79bc5a0fddba747
Abhinav Kumar <abhinav.kr44@gmail.com>
2020-06-28 14:56:24 +0530

    Updates

---
+++ precache-manifest.a07900d39802ab4ec8be129518119378.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9852bbff742a22d254e5d43a4aa7db95",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "887b30439608112adf7b",
+    "url": "/myportfolio/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "d00b15292388766dd21c",
+    "url": "/myportfolio/static/css/main.69de1ab9.chunk.css"
+  },
+  {
+    "revision": "887b30439608112adf7b",
+    "url": "/myportfolio/static/js/2.be222ad2.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/myportfolio/static/js/2.be222ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00b15292388766dd21c",
+    "url": "/myportfolio/static/js/main.1a050e7f.chunk.js"
+  },
+  {
+    "revision": "b8c25f7a87e77086a9c8",
+    "url": "/myportfolio/static/js/runtime-main.bd48ed68.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/myportfolio/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/myportfolio/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "dfa0376a7518806470332c306002e742",
+    "url": "/myportfolio/static/media/myImage.dfa0376a.jpg"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/myportfolio/static/media/todolist.ac82d97d.png"
+  }
+]);
